c52319bf3e3600206da09923fca37ca023952e64
asadr381 <101998038+asadr381@users.noreply.github.com>
2025-02-12 17:24:10 +0500

    first commit

--- src/components/Shipments.js
+++ src/components/Shipments.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import './Shipments.css';
 import { motion } from 'framer-motion';
 import ReCAPTCHA from 'react-google-recaptcha';
+import guide from '../img/guide.PNG';
 
 // Frappe URL and API credentials
 const FRAPPE_URL = process.env.REACT_APP_FRAPPE_URL;
@@ -123,8 +124,122 @@ function Shipments() {
   const onRecaptchaChange = (value) => {
     setRecaptchaValue(value);
   };
+  const [isHovered, setIsHovered] = useState(false);
 
-  return (
+  return ( <div> 
+   
+
+
+   <div 
+      className="shipping-info-container"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <h2 className="shipping-info-heading">Tariff Guide </h2>
+      <motion.img
+  src={guide}
+  alt="Business Growth"
+  className="hero-image"
+  initial={{ opacity: 0 }}
+  animate={{ opacity: 1 }}
+  transition={{ duration: 1, delay: 0.5 }}
+  style={{
+    display: "block",
+    margin: "0 auto",
+    width: "100%",
+    borderRadius: "8px",
+  }}
+/>
+
+      {isHovered && (
+        <div className="shipping-info-details">
+          <p>
+            <strong>Zone</strong><br />
+            To determine the zone number, see Worldwide zones.
+          </p>
+          <p>
+            <strong>Billable weight – for small package shipments</strong><br />
+            Billable weight is the weight used to calculate the rate. For international
+            services, the billable weight will be the greater of the dimensional weight as
+            compared to the actual weight.
+          </p>
+          <p>
+            <strong>How To Calculate Billable Weight</strong><br />
+            Determine Actual Weight: Use any standard scale to determine the actual
+            weight of your package. Increase any fraction to the next half kilogram.<br />
+            Determine Dimensional Weight: Divide the cubic size of your package 
+            (length x width x height, rounding each measurement to the nearest whole 
+            centimeter) in centimeters by 5,000. Refer to the figure at the bottom of this 
+            page. Increase any fraction to the next half kilogram.<br />
+            Determine Billable Weight: Compare the package’s actual weight to its 
+            dimensional weight. The larger of the two weights is the billable weight and 
+            should be used to calculate the rate.<br />
+            For multiple-package shipments, total the billable weight of all packages in 
+            the shipment.
+          </p>
+          <p>
+            All rates exclude any taxes, duties and surcharges that may be imposed pursuant 
+            to local regulations.
+          </p>
+          <p>
+            <strong>Arrange a collection</strong><br />
+            Select “Schedule a Collection” on the Pakistan homepage at www.ups.com or 
+            call UPS on +92-21-111-669-877.
+          </p>
+          <p>
+            <strong>UPS Express Envelope</strong><br />
+            For correspondence and documents only. There is no limit on weight or 
+            number of pages you may enclose in a UPS Express Envelope.
+          </p>
+          <p>
+            <strong>Letter weight limit</strong><br />
+            Letter rates apply for international documents. There is a weight limit of 0.5kg 
+            for documents enclosed in an Express Envelope. Document or Non-Document 
+            rates will apply for envelopes weighing over 0.5kg.
+          </p>
+          <p>
+            <strong>Maximum weight</strong><br />
+            The maximum weight for an individual package is 70kg. If you need to ship 
+            single packages in excess of 70kg, please refer to our UPS Worldwide Express 
+            Freight service or call UPS on +92-21-111-669-877.
+          </p>
+          <p>
+            To determine the rate for a shipment weighing in total more than 70kg, 200kg 
+            (UPS Express Saver) or 100kg (UPS Expedited), add the total billable weight by 
+            the appropriate price shown.
+          </p>
+          <p>
+            The amount billed will be the result of the calculation, or the minimum rate 
+            shown, whichever is the greater.
+          </p>
+          <p>
+            <strong>Additional Information:</strong><br />
+            Weight and Size Limits<br />
+            • Maximum weight per package is 70kg.<br />
+            • Maximum length per package is 270cm.<br />
+            • Maximum size per package is 419cm in length and girth [(2 x width) + 
+            (2 x height)] combined.<br />
+            • There is no limit to the total shipment weight or number of packages in a 
+            shipment.
+          </p>
+        </div>
+      )}
+    </div>
+  
+
+   
+
+
+
+
+
+
+
+   
+
+
+
+    
     <div className="shipments">
       <h2>Submit Your Shipment Query</h2>
       <p>Our team will contact you shortly.</p>
@@ -260,6 +375,8 @@ function Shipments() {
           </a>
         </motion.div>
       </div>
+    
+    </div>
     </div>
   );
 }
